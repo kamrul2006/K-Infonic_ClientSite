821c4989533576800c4576ef68931d26f7c08b4f
kamrul2006 <kamrulislamapurba@gmail.com>
2025-05-05 23:33:52 +0600

    Error page and daisy ui set up done

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import RootLayOut from "./LayOuts/RootlayOut";
+import ErrorPage from "./Components/Fixed/ErrorPage";
 
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <RootLayOut />,
+        errorElement: <ErrorPage />
     }
 ]);
 
